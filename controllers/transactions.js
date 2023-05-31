23483a708cfbc77ed4fc55abe6ab2a2e85eac8b9
Jonathan_DiMarco <jonathandimarcoq@gmail.com>
2023-05-31 23:09:52 +0200

    added DELETE TRANSACTION route

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -46,11 +46,14 @@ const createTransaction = async (req, res) => {
     try {
         const checkFromEnvelope = await db.query(fromEnvelope, [from_id]);
         const checkToEnvelope = await db.query(toEnvelope, [to_id]);
-        if (checkFromEnvelope.rowCount < 1) {
+        if(checkFromEnvelope.rowCount < 1) {
             return res.status(404).send("Envelope to sustract not found!");
         }
-        if (checkToEnvelope.rowCount < 1) {
+        if(checkToEnvelope.rowCount < 1) {
             return res.status(404).send("Envelope to add Not Found!");
+        }
+        if(from_id === to_id) {
+          return res.status(404).send("You can't transfer money into the same envelope!");
         }
         if(checkFromEnvelope.rows[0].budget < amount) {
             return res.status(404).send("insufficient Founds in Envelope!");
@@ -66,4 +69,31 @@ const createTransaction = async (req, res) => {
   }
 };
 
-  module.exports = { getTransactions, getTransactionById, createTransaction};
+//Delete a Transaction
+const deleteTransaction = async (req, res) => {
+
+const id = parseInt(req.params.id);
+const transactionQuery = "SELECT * FROM transactions WHERE id = $1"
+const amountQuery = "SELECT amount FROM transactions WHERE id = $1"
+const updateFromEnvelopeQuery = `UPDATE envelopes SET budget = budget + $1 
+WHERE id IN (SELECT from_id FROM transactions WHERE id = $2)`
+const updateToEnvelopeQuery = `UPDATE envelopes SET budget = budget - $1 
+WHERE id IN (SELECT to_id FROM transactions WHERE id = $2)`
+const deleteTransactionQuery = "DELETE FROM transactions WHERE id=$1";
+
+try {
+  const transaction = await db.query(transactionQuery, [id]);
+  if(transaction.rowCount < 1) {
+    res.status(404).send("Transaction Not Found!");
+  }
+  const amount = await db.query(amountQuery, [id]);
+  await db.query(updateFromEnvelopeQuery, [amount.rows[0].amount, id]);
+  await db.query(updateToEnvelopeQuery, [amount.rows[0].amount, id]);
+  await db.query(deleteTransactionQuery, [id]);
+  res.status(204).send("Transaction deleted succesfully");
+} catch(err) {
+  res.status(500).send({error: err.message});
+}
+};
+
+module.exports = { getTransactions, getTransactionById, createTransaction, deleteTransaction};
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,9 +1,10 @@
 const transactionsRouter = require("express").Router();
-const { getTransactions, getTransactionById, createTransaction } = require("../controllers/transactions.js");
+const { getTransactions, getTransactionById, createTransaction, deleteTransaction } = require("../controllers/transactions.js");
 
 transactionsRouter.get("/", getTransactions);
 transactionsRouter.get("/:id", getTransactionById);
 transactionsRouter.post("/", createTransaction);
+transactionsRouter.delete("/:id", deleteTransaction);
 
 
 
